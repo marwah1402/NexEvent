8ad4873569702cd9a51505983d85adf832895c1b
Marwah Mahbub <marwahmahbub@gmail.com>
2024-12-07 22:53:28 -0500

    Updated deployment paths

--- NexEvent/client/vite.config.js
+++ NexEvent/client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist/app', // Ensure this aligns with your backend folder structure
+  },
+  server: {
+    port: 5173, // Your development server port
+  },
 })
